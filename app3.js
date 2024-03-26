b3cf888a3d862e0d8aace146f403b3b553b90703
RajvanshMalhotra <136916006+RajvanshMalhotra@users.noreply.github.com>
2024-03-26 21:42:05 +0530

    Update app3.js

--- app3.js
+++ app3.js
@@ -3,8 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault(); 
         const query = document.getElementById('search').value;
         if (query.trim() !== '') {
-           window.open(`https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`, '_blank');
-           window.open(`https://www.google.com/search?q=${encodeURIComponent(query)}`, '_blank');
+           
+            window.open(`https://www.youtube.com/results?search_query=${encodeURIComponent(query)}`, '_blank');
+            
+            
+            window.open(`https://www.google.com/search?q=${encodeURIComponent(query)}`, '_blank');
         }
     });
 });
+
